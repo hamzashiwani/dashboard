eed2a25ffa5c5cbe8b25a6fd9de30532875cde13
Hamza Shiwani <hamza.shiwani@solspro.com>
2024-10-17 17:05:42 +0500

    some changes

--- resources/views/admin/client/show.blade.php
+++ resources/views/admin/client/show.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Contact Us Details <i class="feather icon-film"></i></h4>
+                    <h4 class="card-title">Client Details <i class="feather icon-film"></i></h4>
                     <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
                     <div class="heading-elements">
                         <ul class="list-inline mb-0">
